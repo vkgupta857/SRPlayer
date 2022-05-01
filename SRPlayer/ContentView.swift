65fe77eb4d81964d555f14aa518e6d8568ec345b
Vinod Gupta <vkgupta857@gmail.com>
2022-05-01 11:51:16 +0530

    Initial Commit

---
+++ SRPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SRPlayer
+//
+//  Created on 01/05/22.
+//  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SRPlayer/SRPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRPlayerApp.swift
+//  SRPlayer
+//
+//  Created on 01/05/22.
+//  
+
+import SwiftUI
+
+@main
+struct SRPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
